dd5acf6e9c06de4ce45b2e096f6b2dfca5dddf22
Michelle Fernandez <michelle.austria.fernandez@gmail.com>
2013-07-31 22:07:24 -0400

    added signup vc

---
+++ TradeUp/TUSignUpViewController.h
@@ -0,0 +1,13 @@
+//
+//  TUSignUpViewController.h
+//  TradeUp
+//
+//  Created by Michelle Austria on 7/31/13.
+//  Copyright (c) 2013 Michelle Austria. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TUSignUpViewController : UIViewController
+
+@end
---
+++ TradeUp/TUSignUpViewController.m
@@ -0,0 +1,38 @@
+//
+//  TUSignUpViewController.m
+//  TradeUp
+//
+//  Created by Michelle Austria on 7/31/13.
+//  Copyright (c) 2013 Michelle Austria. All rights reserved.
+//
+
+#import "TUSignUpViewController.h"
+
+@interface TUSignUpViewController ()
+
+@end
+
+@implementation TUSignUpViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
