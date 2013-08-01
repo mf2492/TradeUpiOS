71530ad85c85fbb3e053d3ff9a3fdae5420bda4f
Michelle Fernandez <michelle.austria.fernandez@gmail.com>
2013-08-01 18:15:10 -0400

    added more to questions class

--- TradeUp/Question.h
+++ TradeUp/Question.h
@@ -10,11 +10,29 @@
 
 @interface Question : NSObject
 
-@property (strong, nonatomic) NSString *question;
+@property (strong, nonatomic) NSString *prompt;
+@property (strong, nonatomic) NSString *questionID;
+
+
 @property (strong, nonatomic) NSString *answer1;
 @property (strong, nonatomic) NSString *answer2;
 @property (strong, nonatomic) NSString *answer3;
 @property (strong, nonatomic) NSString *answer4;
+
+@property (strong, nonatomic) NSString *id1;
+@property (strong, nonatomic) NSString *id2;
+@property (strong, nonatomic) NSString *id3;
+@property (strong, nonatomic) NSString *id4;
+
+@property (nonatomic) BOOL *isCorrect1;
+@property (nonatomic) BOOL *isCorrect2;
+@property (nonatomic) BOOL *isCorrect3;
+@property (nonatomic) BOOL *isCorrect4;
+
+@property (nonatomic) float *time;
+
+
+
 
 
 
